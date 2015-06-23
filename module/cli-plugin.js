311941d13842a63fc154ac8a9727a6d9f22c602f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-23 16:39:42 +0200

    Read args; find the readme softly

--- module/cli-plugin.js
+++ module/cli-plugin.js
@@ -1,15 +1,43 @@
 import inject from './index';
 
-const {readFileSync, writeFileSync} = require('fs');
+const {readFileSync, writeFileSync, readdirSync} = require('fs');
 const {resolve} = require('path');
 
-export default () => (data) => {
-  const filePath = resolve(process.cwd(), 'Readme.md');
-  const input = readFileSync(filePath, {encoding: 'utf8'});
+const defined = require('defined');
+const find = require('array-find');
+const match = require('1-liners/curry')(require('1-liners/match'));
 
-  const result = inject({input})(data);
+const isReadme = match(/readme.(?:md|html)/i);
 
-  writeFileSync(filePath, result['doxie.inject'].output);
+export default (...args) => {
 
-  return result;
+  // Read options.
+  const options = {};
+  {
+    let i = 0;
+    let key, value;
+    while (
+      typeof (key = args[i++]) === 'string' &&
+      typeof (value = args[i++]) === 'string'
+    ) options[key] = value;
+  }
+
+  // Determine the path of the document.
+  const cwd = process.cwd();
+  const filePath = resolve(cwd, defined(options.into,
+    find(readdirSync(cwd), isReadme)
+  ));
+
+  return (data) => {
+
+    // Read and process the document.
+    const input = readFileSync(filePath, {encoding: 'utf8'});
+    const result = inject({input})(data);
+
+    // Overwrite the document.
+    writeFileSync(filePath, result['doxie.inject'].output);
+
+    // Return the `result`.
+    return result;
+  };
 };
