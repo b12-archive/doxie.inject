af8c4682d4426601e9968e65ed3bb3953af8820c
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-23 16:52:33 +0200

    Specify `into`

--- test/cli-plugin.test.js
+++ test/cli-plugin.test.js
@@ -14,11 +14,6 @@ const cp = (source, destination) => {
   cpFile.sync(resolve(cwd, source), resolve(cwd, destination));
 };
 
-const mock = dummyOutput([
-  '##  Function 1  ##\n\n',
-  '##  Function 2  ##\n\n',
-]);
-
 const read = (filename) => readFileSync(
   resolve(cwd, filename),
   {encoding: 'utf8'}
@@ -32,7 +27,11 @@ test(title('Works as expected'), (is) => {
   process.cwd = mockCwd;
 
   cp('Readme.md', 'Readme.md.backup');
-  cli()(mock);
+  cli()(dummyOutput([
+    '##  Function 1  ##\n\n',
+    '##  Function 2  ##\n\n',
+  ]));
+
   is.equal(
     read('Readme.md'),
 `My readme
@@ -58,7 +57,42 @@ License
     ,
     'without parameters'
   );
+
   cp('Readme.md.backup', 'Readme.md');
+
+  cp('Docs.html', 'Docs.html.backup');
+  cli('into', 'Docs.html')(dummyOutput([
+    '<h3>Function 1</h3>\n\n',
+    '<h3>Function 2</h3>\n\n',
+  ]));
+
+  is.equal(
+    read('Docs.html'),
+`<h1>My super-duper fancy docs</h1>
+
+<h2>Public API</h2>
+<!-- @doxie.inject start -->
+<!-- Don’t remove or change the comment above – that can break automatic updates. -->
+<h3>Function 1</h3>
+
+<h3>Function 2</h3>
+
+<!-- Don’t remove or change the comment below – that can break automatic updates. More info at <http://npm.im/doxie.inject>. -->
+<!-- @doxie.inject end -->
+
+<h2>Private API</h2>
+<!-- @doxie.inject start private -->
+…
+<!-- @doxie.inject end private -->
+
+<h2>License</h2>
+<p>MIT</p>
+`
+    ,
+    '`into` a custom file'
+  );
+
+  cp('Docs.html.backup', 'Docs.html');
 
   process.cwd = originalCwd;
   is.end();
