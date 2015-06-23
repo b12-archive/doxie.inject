1d7fcea5664acad1a1563557c6533894c74ff0a7
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-23 15:11:03 +0200

    Implement named markers

--- module/index.js
+++ module/index.js
@@ -6,17 +6,30 @@ const assign = require('object-assign');
 const prefix = _prefix('doxie.input');
 const injectError = require('tiny-error')({prefix});
 
-export default ({input} = {}) => {
+export default ({
+  input,
+  as = null,  /* jshint ignore: line */
+} = {}) => {
   if (typeof input === 'undefined') throw injectError(
     'Wrong parameters. Pass an `{Object} parameters` with a `{String} ' +
     'parameters.input`.'
   );
 
   input = String(input);
+  if (as !== null) as = String(as);
 
-  const betweenMarkers =
-    /(<!--\s*@doxie\.inject\s+start\s*-->)[^]*(<!--\s*@doxie\.inject\s+end\s*-->)/g
-  ;
+  const markerName = (as !== null ?
+    `\\s*${as}` :
+    ''
+  );
+
+  const marker = (role) => (
+    `<!--\\s*@doxie\\.inject\\s+${role}${markerName}\\s*-->`
+  );
+
+  const betweenMarkers = new RegExp(
+    `(${marker('start')})[^]*(${marker('end')})`
+  );
 
   return (data) => {
     let error = null;
