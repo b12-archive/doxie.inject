5b3b29bd1f95ed24217de5679b7c1541ccc8939d
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-23 09:30:44 +0200

    Specify graceful failure

--- test/index.test.js
+++ test/index.test.js
@@ -5,14 +5,18 @@ const test = require('tape-catch');
 const title = require('1-liners/curry')(require('1-liners/plus'))(
   'Programmatic API:  '
 );
+const curry = require('1-liners/curry');
+const last = require('1-liners/last');
+const property = curry(require('1-liners/property'));
+const match = curry(require('1-liners/match'));
+
+const dummyData = dummyOutput([
+  'abc',
+  'def',
+]);
 
 test(title('Injects docs between default markers'), (is) => {
-  const data = dummyOutput([
-    'abc',
-    'def',
-  ]);
-
-  const result = (input) => inject({input})(data)['doxie.inject'].output;
+  const result = (input) => inject({input})(dummyData)['doxie.inject'].output;
 
   is.equal(
     result(
@@ -44,22 +48,39 @@ My footer
     'keeping all odd whitespace'
   );
 
+  is.end();
+});
+
+test(title('Fails gracefully when the input content is wrong'), (is) => {
+  const result = (input) => inject({input})(dummyData);
+  const results = [];
+
+  results.push(result('My readme'));
   is.equal(
-    result('My readme'),
+    last(results)['doxie.inject'].output,
     'My readme',
     'not changing anything if there are no markers in the readme, …'
   );
 
+  results.push(result('stuff <!-- @doxie.inject start --> stuff'));
   is.equal(
-    result('stuff <!-- @doxie.inject start --> stuff'),
+    last(results)['doxie.inject'].output,
     'stuff <!-- @doxie.inject start --> stuff',
     '…or if the end marker isn’t there, …'
   );
 
+  results.push(result('stuff <!-- @doxie.inject end --> stuff'));
   is.equal(
-    result('stuff <!-- @doxie.inject end --> stuff'),
+    last(results)['doxie.inject'].output,
     'stuff <!-- @doxie.inject end --> stuff',
     '…or if the start marker isn’t there'
+  );
+
+  is.ok(
+    results.map(property('error')).every(
+      match(/markers not found/i)
+    ),
+    'printing a helpful error'
   );
 
   is.end();
