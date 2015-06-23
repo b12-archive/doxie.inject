892fc501b48f422cad46fc62d892db4f8925c327
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-23 09:44:28 +0200

    Satisfy the spec

--- module/index.js
+++ module/index.js
@@ -4,12 +4,32 @@ const assign = require('object-assign');
 
 export default ({input}) => {
   // TODO: Check input
-  return (data) => assign({}, data, {
-    ['doxie.inject']: {
-      output: input.replace(
-        /(<!--\s*@doxie\.inject\s+start\s*-->)[^]*(<!--\s*@doxie\.inject\s+end\s*-->)/g,
+
+  const betweenMarkers =
+    /(<!--\s*@doxie\.inject\s+start\s*-->)[^]*(<!--\s*@doxie\.inject\s+end\s*-->)/g
+  ;
+
+  return (data) => {
+    let error = null;
+    let output;
+
+    if (!betweenMarkers.test(input)) {
+      output = input;
+      error = {error:
+        '[doxie --inject] Warning: Markers not found. Make sure you have a ' +
+        '`<!-- @doxie.inject start -->` followed by a `<!-- @doxie.inject ' +
+        'end -->` in your document.'
+      };
+    } else {
+      output = input.replace(
+        betweenMarkers,
         `$1${data.docs.map(property('output')).join('')}$2`
-      ),
-    },
-  });
+      );
+    }
+
+    return assign({}, data,
+      {['doxie.inject']: {output}},
+      error
+    );
+  };
 };
